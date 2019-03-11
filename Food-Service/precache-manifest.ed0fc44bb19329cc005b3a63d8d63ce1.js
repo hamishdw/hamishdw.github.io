852e91d87106c865b54f4bcff9ce3649f46e664c
Hamish.Webb <>
2019-03-11 16:54:33 +1300

    no message

---
+++ Food-Service/precache-manifest.ed0fc44bb19329cc005b3a63d8d63ce1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7f8b0ea432f3528908c6",
+    "url": "./static/js/main.4602022c.chunk.js"
+  },
+  {
+    "revision": "4bd3fc68cf9e8e59da46",
+    "url": "./static/js/2.52e305b8.chunk.js"
+  },
+  {
+    "revision": "7f8b0ea432f3528908c6",
+    "url": "./static/css/main.c50086e9.chunk.css"
+  },
+  {
+    "revision": "afd9cffbc24e4e41e79324132588c770",
+    "url": "./index.html"
+  }
+];
