5d68899739540fd070194f2ecbd896cadc177c75
Hamish.Webb <>
2019-03-12 09:08:33 +1300

    no message

---
+++ Food-Service/service-worker-custom.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', () => {
+  console.log('its alive!');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+  self.clients.matchAll({ type: 'window' }).then(windowClients => {
+    for (let windowClient of windowClients) {
+      // Force open pages to refresh, so that they have a chance to load the
+      // fresh navigation response from the local dev server.
+      windowClient.navigate(windowClient.url);
+    }
+  });
+});
