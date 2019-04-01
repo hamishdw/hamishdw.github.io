d001a3deecbde72dac264e0464d1799a6f922f83
Hamish.Webb <>
2019-04-01 13:59:17 +1300

    no message

--- Food-Service/service-worker.js
+++ Food-Service/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["static/css/main.f1ca5855.css","93eadcdf086a5fedf139bea7a63b3927"],["static/js/main.488e5f9c.js","738ebc32c2a3f5caafbb550ab5e21c77"],["static/media/DalaMoa-Medium.1ffc0988.otf","1ffc0988edca07d37e20b0cbcabf9196"],["static/media/Pitch-Light.6956ca30.otf","6956ca30acb1e3a171b13f45524e2431"],["static/media/slider.9537dae6.svg","9537dae604b2936694dbf895de433147"]];
+var precacheConfig = [["static/css/main.cfce0380.css","1911ccf8b2bf62049f7a1f2a07a55559"],["static/js/main.3ee05e27.js","9be71ef09ce95f6ba15bcb74148bc30a"],["static/media/DalaMoa-Medium.1ffc0988.otf","1ffc0988edca07d37e20b0cbcabf9196"],["static/media/Pitch-Light.6956ca30.otf","6956ca30acb1e3a171b13f45524e2431"],["static/media/slider.9537dae6.svg","9537dae604b2936694dbf895de433147"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
