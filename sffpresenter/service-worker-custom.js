3f9d16d4477c6d3bbcd8efed4239f004d704011e
Hamish.Webb <>
2019-04-09 10:41:29 +1200

    no message

--- sffpresenter/service-worker-custom.js
+++ sffpresenter/service-worker-custom.js
@@ -1,136 +1,96 @@
-// This service worker file is effectively a 'no-op' that will reset any
-// previous service worker registered for the same host:port combination.
-// In the production build, this file is replaced with an actual service worker
-// file that will precache your site's local assets.
-// See https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
+var CACHE_VERSION = 2;
+var CURRENT_CACHES = {
+  prefetch: 'prefetch-cache-v' + CACHE_VERSION
+};
 
-var CACHE_NAME = 'my-site-cache-v5';
-// var urlsToCache = [
-//   '/static/',
-//   '/images/',
-//   '/apple-icon-60x60.png',
-//   '/asset-manifest.json',
-//   '/icon.png',
-//   '/index.html',
-//   '/manifest.json',
-//   '/service-worker-custom.js',
-//   '/videos/video2.mp4',
-//   '/videos/video3.mp4',
-//   '/videos/video4.mp4',
-//   '/videos/sustainability-big.mp4'
-// ];
+self.addEventListener('install', function(event) {
+  var urlsToPrefetch = [
+    './'
+  ];
 
-var urlsToCache = [
-  './'
-];
+  // All of these logging statements should be visible via the "Inspect" interface
+  // for the relevant SW accessed via chrome://serviceworker-internals
+  console.log('Handling install event. Resources to prefetch:', urlsToPrefetch);
 
-self.addEventListener('activate', event => {
-  self.clients.matchAll({ type: 'window' }).then(windowClients => {
-    for (let windowClient of windowClients) {
-      // Force open pages to refresh, so that they have a chance to load the
-      // fresh navigation response from the local dev server.
-      windowClient.navigate(windowClient.url);
-    }
-  });
+  self.skipWaiting();
 
   event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (CACHE_NAME.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
+    caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
+      return cache.addAll(urlsToPrefetch);
     })
   );
 });
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+self.addEventListener('activate', function(event) {
+  // Delete all caches that aren't named in CURRENT_CACHES.
+  // While there is only one cache in this example, the same logic will handle the case where
+  // there are multiple versioned caches.
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
+  });
+
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
+            console.log('Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+        );
+    })
+    );
 });
 
-//This code is based on  https://googlechrome.github.io/samples/service-worker/prefetch-video/ 
-
 self.addEventListener('fetch', function(event) {
-
-  // console.log('event',event);
-  
-  headersLog = [];
-  for (var pair of event.request.headers.entries()) {
-    // console.log(pair[0]+ ': '+ pair[1]);
-    headersLog.push(pair[0]+ ': '+ pair[1])
- }
-//  console.log('Handling fetch event for', event.request.url, JSON.stringify(headersLog));
+  console.log('Handling fetch event for', event.request.url);
 
   if (event.request.headers.get('range')) {
-    // console.log('Range request for', event.request.url);
-    var rangeHeader=event.request.headers.get('range');
-    var rangeMatch =rangeHeader.match(/^bytes\=(\d+)\-(\d+)?/)
-    var pos =Number(rangeMatch[1]);
-    var pos2=rangeMatch[2];
-    if (pos2) { pos2=Number(pos2); }
-    
-    // console.log('Range request for '+ event.request.url,'Range: '+rangeHeader, "Parsed as: "+pos+"-"+pos2);
+    var pos =
+    Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
+    console.log('Range request for', event.request.url,
+      ', starting position:', pos);
     event.respondWith(
-      caches.open(CACHE_NAME)
+      caches.open(CURRENT_CACHES.prefetch)
       .then(function(cache) {
         return cache.match(event.request.url);
       }).then(function(res) {
         if (!res) {
-          // console.log("Not found in cache - doing fetch")
           return fetch(event.request)
           .then(res => {
-            // console.log("Fetch done - returning response ",res)
             return res.arrayBuffer();
           });
         }
-        // console.log("FOUND in cache - doing fetch")
         return res.arrayBuffer();
       }).then(function(ab) {
-        // console.log("Response procssing")
-        let responseHeaders=  {
-          status: 206,
-          statusText: 'Partial Content',
-          headers: [
-            ['Content-Type', 'video/mp4'],
-            ['Content-Range', 'bytes ' + pos + '-' + 
-            (pos2||(ab.byteLength - 1)) + '/' + ab.byteLength]]
-        };
-        
-        // console.log("Response: ",JSON.stringify(responseHeaders))
-        var abSliced={};
-        if (pos2>0){
-          abSliced=ab.slice(pos,pos2+1);
-        }else{
-          abSliced=ab.slice(pos);
-        }
-        
-        // console.log("Response length: ",abSliced.byteLength)
         return new Response(
-          abSliced,responseHeaders
-        );
+          ab.slice(pos),
+          {
+            status: 206,
+            statusText: 'Partial Content',
+            headers: [
+              // ['Content-Type', 'video/webm'],
+              ['Content-Range', 'bytes ' + pos + '-' +
+                (ab.byteLength - 1) + '/' + ab.byteLength]]
+          });
       }));
   } else {
-    // console.log('Non-range request for', event.request.url);
+    console.log('Non-range request for', event.request.url);
     event.respondWith(
     // caches.match() will look for a cache entry in all of the caches available to the service worker.
     // It's an alternative to first opening a specific named cache and then matching on that.
     caches.match(event.request).then(function(response) {
       if (response) {
-        // console.log('Found response in cache:', response);
+        console.log('Found response in cache:', response);
         return response;
       }
-      // console.log('No response found in cache. About to fetch from network...');
+      console.log('No response found in cache. About to fetch from network...');
       // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
       // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
       return fetch(event.request).then(function(response) {
-        // console.log('Response from network is:', response);
+        console.log('Response from network is:', response);
 
         return response;
       }).catch(function(error) {
