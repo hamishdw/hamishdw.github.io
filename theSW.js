b1ef6d69c117b7bccf1ae9105c68b0a8dc9969ef
Hamish.Webb <>
2019-07-16 17:08:33 +1200

    no message

--- theSW.js
+++ theSW.js
@@ -1,6 +1,6 @@
 // THANKS - Phil Nash https://github.com/philnash/philna.sh/blob/ba798a2d5d8364fc7c1dae1819cbd8ef103c8b67/sw.js#L50-L94
 
-var version = "1.1.15";
+var version = "1.1.16";
 
 var staticCacheName = "ver-"+version;
 const staticAssets = [
@@ -53,6 +53,7 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  self.skipWaiting();
   var url = new URL(event.request.url);
 
   if (url.pathname.match(/^\/((images|videos|test)\/|manifest.json$)/)) {
