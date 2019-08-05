007b455efb9528467e0d122ed315b3e8fe287319
Hamish.Webb <>
2019-08-06 11:54:27 +1200

    trying CM axios

---
+++ precache-manifest.24c84d3302499bc63d714ffd92b2d04d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db648568f3c01e062c9aac3e91a3fe5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aeb77709837b96abe104",
+    "url": "./static/css/main.dd201f81.chunk.css"
+  },
+  {
+    "revision": "71aea844324e3a50b1e1",
+    "url": "./static/js/2.3e1294d3.chunk.js"
+  },
+  {
+    "revision": "aeb77709837b96abe104",
+    "url": "./static/js/main.0585f465.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- sffpresenter/static/js/0.d57b303b.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],{65:function(e,p){e.exports={test:"yep"}}});
-//# sourceMappingURL=0.d57b303b.chunk.js.map
