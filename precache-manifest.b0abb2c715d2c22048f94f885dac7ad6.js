b3f36ca368ccb0bb9ad84c27f53ec52e43f594e6
Hamish <hamish.webb@hotmail.co.nz>
2020-03-23 16:22:16 +1300

    no message

---
+++ precache-manifest.b0abb2c715d2c22048f94f885dac7ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04aac6cebe670c716b37e8bc3eb3ffe8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ebb299754d4d8d1da6d",
+    "url": "./static/css/main.48cdb791.chunk.css"
+  },
+  {
+    "revision": "253a2313fa3ce4cd9489",
+    "url": "./static/js/2.07b01bf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.07b01bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ebb299754d4d8d1da6d",
+    "url": "./static/js/main.60041a38.chunk.js"
+  },
+  {
+    "revision": "2a4e5a752bdc207a6a80",
+    "url": "./static/js/runtime-main.de1d4ce9.js"
+  }
+]);
